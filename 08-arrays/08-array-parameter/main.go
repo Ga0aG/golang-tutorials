1eeb9fe72dec63fe11ddd76e62aabfd241ed1a97
zhuojun.lai <zhuojun.lai@dorabot.com>
2025-12-22 09:11:14 +0800

    Update array

--- 08-arrays/08-array-parameter/main.go
+++ 08-arrays/08-array-parameter/main.go
@@ -5,10 +5,19 @@ func main() {
 	array := [3]float64{7.0, 8.5, 9.1}
 	x := Sum(&array) // Note the explicit address-of operator
 	// to pass a pointer to the array
-	fmt.Printf("The sum of the array is: %f", x)
+	fmt.Printf("The sum of the array is: %f\n", x)
+	fmt.Printf("The sum of the array is: %f", Sum2(array))
 }
 
 func Sum(a *[3]float64) (sum float64) {
+	for _, v := range a { // derefencing *a to get back to the array is not necessary!
+		sum += v
+	}
+	return
+}
+
+// 传值（默认行为）- 会产生复制; 切片的话可以直接使用切片
+func Sum2(a [3]float64) (sum float64) {
 	for _, v := range a { // derefencing *a to get back to the array is not necessary!
 		sum += v
 	}
